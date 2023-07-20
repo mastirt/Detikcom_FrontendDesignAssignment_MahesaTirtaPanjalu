b555d9d9221f25f69bf7134a67141c2f0ddfa134
mahesa tirta panjalu <mahesat8@gmail.com>
2023-07-20 22:22:11 +0700

    Add folder via upload
    add the created folder

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+$('form').on('change', '.file-upload-field', function () {
+  $(this)
+    .parent('.file-upload-wrapper')
+    .attr(
+      'data-text',
+      $(this)
+        .val()
+        .replace(/.*(\/|\\)/, '')
+    );
+});
+
+
